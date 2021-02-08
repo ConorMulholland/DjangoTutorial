ab155787e20e262a7422ff5d5900887440e80bb8
ConorMulholland <75023894+ConorMulholland@users.noreply.github.com>
2021-02-08 15:54:02 +0000

    t

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,8 +6,17 @@ from django.urls import reverse
 from django.views import generic
 
 from .models import Question
+from django.contrib.auth import authenticate, login
 
-
+def my_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+    else:
+        print "Invalid login"
+        
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
