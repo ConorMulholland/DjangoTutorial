daa690e6b61228b5ea4a3a6089a24bbec377ba82
ConorMulholland <75023894+ConorMulholland@users.noreply.github.com>
2021-02-09 16:31:05 +0000

    Creating the login page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,15 +7,6 @@ from django.views import generic
 
 from .models import Question
 from django.contrib.auth import authenticate, login
-
-def my_view(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-    else:
-        print "Invalid login"
         
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
