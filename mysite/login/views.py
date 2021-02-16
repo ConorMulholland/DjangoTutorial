1b3e382ee6a19d3dccd5b044125df3f93b13d714
ConorMulholland <75023894+ConorMulholland@users.noreply.github.com>
2021-02-16 15:27:50 +0000

    Login page

--- mysite/login/views.py
+++ mysite/login/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth import authenticate, login
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
     return render(request, 'login/login.html')
+
+def myview(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return render(request, 'login/Welcome.html')
+    else:
+        return HttpResponseRedirect(reverse('login:index'))
